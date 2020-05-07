a5580c7353aeacd80716936cf087ba38e7182708
myh0rizon <msshantamohashin@gmail.com>
2020-05-07 16:38:58 +0100

    Github-Finder[Part-5]-Fetch-&-Display -Repos

--- js-api-github-finder/github.js
+++ js-api-github-finder/github.js
@@ -2,6 +2,8 @@ class GitHub {
   constructor() {
     this.client_id = "6c83ce5ed62152fa708b";
     this.client_secret = "b7a5d0840f735bd7a58a90f0dc18360583b069eb";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
   }
 
   async getUser(user) {
@@ -9,10 +11,16 @@ class GitHub {
       `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
     );
 
+    const reposResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
     const profile = await profileResponse.json();
+    const repos = await reposResponse.json();
 
     return {
       profile,
+      repos,
     };
   }
 }
