9aff3f7ac828645ad02b2e1205f5a0a33d46b705
myh0rizon <msshantamohashin@gmail.com>
2020-05-20 16:27:17 +0100

    Microposts (Webpack & Babel): Post Edit State & Update [1]

--- js-microposts/src/app.js
+++ js-microposts/src/app.js
@@ -10,7 +10,10 @@ document.querySelector(".post-submit").addEventListener("click", submitPost);
 // Listen for delete
 document.querySelector("#posts").addEventListener("click", deletePost);
 
-// Gt posts
+// Listen for edit state
+document.querySelector("#posts").addEventListener("click", enableEdit);
+
+// Get Posts
 function getPosts() {
   http
     .get("http://localhost:3000/posts")
@@ -18,7 +21,7 @@ function getPosts() {
     .catch((err) => console.log(err));
 }
 
-// Submit post
+// Submit Post
 function submitPost() {
   const title = document.querySelector("#title").value;
   const body = document.querySelector("#body").value;
@@ -28,7 +31,7 @@ function submitPost() {
     body,
   };
 
-  // Create post
+  // Create Post
   http
     .post("http://localhost:3000/posts", data)
     .then((data) => {
@@ -39,19 +42,41 @@ function submitPost() {
     .catch((err) => console.log(err));
 }
 
-// Delete post
+// Delete Post
 function deletePost(e) {
-  e.preventDefault();
   if (e.target.parentElement.classList.contains("delete")) {
     const id = e.target.parentElement.dataset.id;
     if (confirm("Are you sure?")) {
       http
         .delete(`http://localhost:3000/posts/${id}`)
         .then((data) => {
-          ui.showAlert("Post Removed", "alert alert-success");
+          ui.showAlert("Post removed", "alert alert-success");
           getPosts();
         })
         .catch((err) => console.log(err));
     }
   }
+  e.preventDefault();
+}
+
+// Enable Edit State
+function enableEdit(e) {
+  if (e.target.parentElement.classList.contains("edit")) {
+    const id = e.target.parentElement.dataset.id;
+    const title =
+      e.target.parentElement.previousElementSibling.previousElementSibling
+        .textContent;
+    const body = e.target.parentElement.previousElementSibling.textContent;
+
+    const data = {
+      id,
+      title,
+      body,
+    };
+
+    // Fill form with current post
+    ui.fillForm(data);
+  }
+
+  e.preventDefault();
 }
