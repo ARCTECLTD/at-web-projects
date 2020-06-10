17b7c1862a01914500ff3eafe2844c352d099f21
myh0rizon <msshantamohashin@gmail.com>
2020-06-10 16:05:17 +0100

    js-es6-weather: ISPLAYING DATA & WRAP UP

--- js-es6-weather/app.js
+++ js-es6-weather/app.js
@@ -11,6 +11,8 @@ function searchWeather() {
   if (CITY_NAME.length == 0) {
     return alert("Please enter a city name");
   }
+  ELEMENTS.ELEMENT_LOADING_TEXT.style.display = "block";
+  ELEMENTS.ELEMENT_WEATHER_BOX.style.display = "NONE";
   const URL =
     "http://api.openweathermap.org/data/2.5/weather?q=" +
     CITY_NAME +
@@ -25,6 +27,16 @@ function searchWeather() {
       );
       const WEATHER_PROXY = new Proxy(WEATHER_DATA, WEATHER_PROXY_HANDLER);
       WEATHER_PROXY.temperature = responseData.main.temp;
+      updateWeather(WEATHER_PROXY);
     })
     .catch((error) => alert(error));
+}
+
+function updateWeather(weatherData) {
+  ELEMENTS.ELEMENT_WEATHER_CITY.textContent = weatherData.cityName;
+  ELEMENTS.ELEMENT_WEATHER_DESCRIPTION.textContent = weatherData.description;
+  ELEMENTS.ELEMENT_WEATHER_TEMPERATURE.textContent = weatherData.temperature;
+
+  ELEMENTS.ELEMENT_LOADING_TEXT.style.display = "none";
+  ELEMENTS.ELEMENT_WEATHER_BOX.style.display = "block";
 }
